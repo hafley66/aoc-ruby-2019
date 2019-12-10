32793b3e9528b529bf38724a77238a78f5ad19cb
Chris Hafley <hafley66@gmail.com>
2019-12-09 21:14:20 -0500

    Almost got day 5 done, realized i was no longer passing indexes, but the values acutally at the index to the op functions Not relizing this caused many bugs until i realized and it started working Gonna get ready for part 2

--- day5/pt1.rb
+++ day5/pt1.rb
@@ -5,145 +5,111 @@ MULTIPLY = 2
 INPUT = 3
 OUTPUT = 4
 
+Log = ->(it) {
+  return
+  puts it.to_s
+}
+
 class IntCodeArray
 
-  def arithmetic_op op_int, modes, x, y, out
-    x_mode, y_mode = modes
-    self[self[out]] = self[x, x_mode].send(
-      case op_int
+  def arithmetic_op(op_int, (mode_x, mode_y), (x, y, out))
+    mode_x ||= 0
+    mode_y ||= 0
+
+    Log.call ["arithmetic_op", {
+      op_int: op_int,
+      mode_x: mode_x,
+      mode_y: mode_y,
+      x: x,
+      y: y,
+      out: out
+    }]
+
+    xx = self[x, mode_x]
+    yy = self[y, mode_y]
+    Log.call ["a2", xx, yy]
+    @codes[out] = xx.send(
+    case op_int
       when ADD
         :+
       when MULTIPLY
         :*
       end,
-      self[y, y_mode]
+      yy
     )
   end
 
-  def io_op op_int, address, inputs, outputs
+  def io_op(op_int, (mode), (arg))
+    mode ||= 0
     case op_int
     when INPUT
-      self[address]
+      puts "Input OP:"
+      @codes[arg] = gets.chomp.to_i
     when OUTPUT
-      self[address, ]
+      puts "Output OP:#{self[arg, mode]}"
     end
   end
 
-
-  def initialize codes, inputs
+  def initialize codes
     @codes = codes
-    @inputs = inputs
   end
 
   def [] (index, mode=POSITION)
-    case mode
+    x = case mode
     when POSITION, nil
-      @codes[@codes[index]]
-    when IMMEDIATE
       @codes[index]
-    end
-  end
-
-  def []= (index, mode=position)
-    case mode
-    when POSITION, nil
-      @codes[@codes[index]]
     when IMMEDIATE
-      @codes[index]
+      index
     end
+    Log.call ["[]", index, mode, x]
+    x
   end
 
   def parse_op_code op_int
     s = op_int.to_s
-    op_i = s[-2..-1].to_i
-    modes = s[0..-2]
+    op_i = ( s[-2..-1] || s[-1] ).to_i
+    modes = s[0...-2]
+    Log.call ["parse", op_int, s, op_i, modes]
     return [
       op_i,
-      modes.reverse
+      (modes.reverse + "00000000000").split("").map(&:to_i)
     ]
   end
 
-  def run_op index, inputs
-
-  end
-
   def call
-    @index = 0
     i = 0
-    push = 0
-    while i = op_code(a, i);;end
-      a[0]
+    while i = op_code(i);;end
+    @codes[0]
+  end
+
+  def op_code index
+    op_i, modes = parse_op_code @codes[index]
+    args = @codes[index+1..]
+
+    Log.call ["op_code:", index, op_i, modes.to_s, op_i]
+
+    push = case op_i
+    when ADD, MULTIPLY
+      arithmetic_op op_i, modes, args
+      4
+    when INPUT, OUTPUT
+      io_op op_i, modes, args
+      2
+    when 99
+      return nil
+    else
+      raise StandardError.new("Unexpected input!#{index} #{@codes[index]}")
     end
-end
 
-# def op_code array, index, inputs
-#   op, a, b, c = array[index...index+4]
-#   code, push = case op
-#   when 1
-#     [:+, 4]
-#   when 2
-#     [:*, 4]
-#   when 3
-#     [:<, 1]
-#   when 4
-#     [:>, 1]
-#   when 99
-#     return nil
-#   else
-#     raise StandardError.new("Unexpected input!"+ ({ index: index, op: op, a: a, b: b, c: c}).to_s)
-#   end
-#
-#
-#   array[c] = array[a].send(code, array[b])
-#   if (index = (index + push)) < array.length
-#     index
-#   else
-#     nil
-#   end
-# end
-
-
-
-def run_ops a
-  i = 0
-  push = 0
-  while i = op_code(a, i);;end
-  a[0]
-end
-
-def init_intcode a, n, v
-  b = [*a]
-  b[1] = n
-  b[2] = v
-  b
-end
-
-def main array, target
-  realm = (realm = [*(0..99)]).product realm
-  realm.find_all do |(n, v)|
-    x = run_ops(
-      init_intcode(array, n, v)
-    ) rescue false
-    puts({x:x, n:n, v:v}.to_s) if x <= target
-    x == target
+    if (index = (index + push)) < @codes.length
+      index
+    else
+      nil
+    end
   end
 end
-
-puts (
-  main(
-    File.read("in.txt").split(",").map(&:to_i),
-    19690720
-  ).to_s
+c = File.read("in.txt").split(",").map(&:to_i)
+p = IntCodeArray.new(
+  c
 )
-
-
-A = Struct.new() do
-  def []=(a,b,c,val)
-    puts (a,b,c,val)
-  end
-end
-
-
-
-a = A.new
-a[1,2,3] = 4
+puts p.call
